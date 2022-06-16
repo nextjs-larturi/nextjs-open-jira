4937829b5a533cd57745e96c0237e40bcb9baf1e
Leandro Arturi <lea.arturi@gmail.com>
2022-06-16 18:37:28 -0300

    Endpoint Edit Entry

---
+++ pages/api/entries/[id].ts
@@ -0,0 +1,51 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import mongoose from 'mongoose';
+import { db } from '../../../database';
+import { Entry } from '../../../models';
+import { iEntry } from '../../../models/Entry';
+
+type Data = 
+    | { message: string }
+    | iEntry;
+
+
+export default function handler (req: NextApiRequest, res: NextApiResponse<Data>) {
+    const { id } = req.query;
+   
+    if(!mongoose.isValidObjectId(id)) {
+        return res.status(400).json({ message: 'Invalid id' });
+    }
+
+    switch (req.method) {
+        case 'PUT':
+            return updateEntry(req, res);
+    
+        default:
+            return res.status(400).json({ message: 'Metodo no existente' });
+    }
+}
+
+const updateEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+    const { id } = req.query;
+
+    await db.connect();
+
+    const entryToUpdate = await Entry.findById(id);
+
+    if (!entryToUpdate) {
+        await db.disconnect();
+        return res.status(400).json({ message: 'No hay entrada con el id ' + id });
+    }
+
+    const { 
+        description = entryToUpdate.description,
+        status = entryToUpdate.status
+    } = req.body;
+
+    const updatedEntry = await Entry.findByIdAndUpdate(id, 
+        { description, status }, 
+        { new: true, runValidators: true }
+    );
+
+    res.status(200).json(updatedEntry!);
+}
