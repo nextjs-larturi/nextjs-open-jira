ac8bf38d3e7a19bc92d17793f17da57f42da14b4
Leandro Arturi <lea.arturi@gmail.com>
2022-06-16 18:45:47 -0300

    Endpoint Edit Entry

--- pages/api/entries/[id].ts
+++ pages/api/entries/[id].ts
@@ -42,10 +42,17 @@ const updateEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
         status = entryToUpdate.status
     } = req.body;
 
-    const updatedEntry = await Entry.findByIdAndUpdate(id, 
-        { description, status }, 
-        { new: true, runValidators: true }
-    );
-
-    res.status(200).json(updatedEntry!);
+    try {
+        const updatedEntry = await Entry.findByIdAndUpdate(id, 
+            { description, status }, 
+            { new: true, runValidators: true }
+        );
+        await db.disconnect();
+        res.status(200).json(updatedEntry!);
+    } catch (error: any) {
+        await db.disconnect();
+        return res.status(400).json({ 
+            message: error.errors.status.message
+        });
+    }
 }
